36d7affc40060020d4dde2a586d21f38bacb3aae
suyh <su_787910081@sina.com>
2026-09-08 21:45:07 +0800

    优化调整

--- src/plugins/auth.js
+++ src/plugins/auth.js
@@ -12,18 +12,6 @@ function authPermission(permission) {
   }
 }
 
-function authRole(role) {
-  const super_admin = "admin"
-  const roles = useUserStore().roles
-  if (role && role.length > 0) {
-    return roles.some(v => {
-      return super_admin === v || v === role
-    })
-  } else {
-    return false
-  }
-}
-
 export default {
   // 验证用户是否具备某权限
   hasPermi(permission) {
@@ -39,22 +27,6 @@ export default {
   hasPermiAnd(permissions) {
     return permissions.every(item => {
       return authPermission(item)
-    })
-  },
-  // 验证用户是否具备某角色
-  hasRole(role) {
-    return authRole(role)
-  },
-  // 验证用户是否含有指定角色，只需包含其中一个
-  hasRoleOr(roles) {
-    return roles.some(item => {
-      return authRole(item)
-    })
-  },
-  // 验证用户是否含有指定角色，必须全部拥有
-  hasRoleAnd(roles) {
-    return roles.every(item => {
-      return authRole(item)
     })
   }
 }
