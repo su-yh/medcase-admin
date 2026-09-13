0ccd8c6a22b15237a7b87f0b26d539ed0a64e801
suyh <su_787910081@sina.com>
2026-09-13 16:44:25 +0800

    枚举替代字典

---
+++ src/constants/system.js
@@ -0,0 +1,25 @@
+export const USER_SEX_OPTIONS = [
+  { label: '男', value: '0', elTagType: '' },
+  { label: '女', value: '1', elTagType: '' },
+  { label: '未知', value: '2', elTagType: '' }
+]
+
+export const NORMAL_DISABLE_OPTIONS = [
+  { label: '正常', value: '0', elTagType: 'primary' },
+  { label: '停用', value: '1', elTagType: 'danger' }
+]
+
+export const YES_NO_OPTIONS = [
+  { label: '是', value: 'Y', elTagType: 'primary' },
+  { label: '否', value: 'N', elTagType: 'danger' }
+]
+
+export const NOTICE_TYPE_OPTIONS = [
+  { label: '通知', value: '1', elTagType: 'warning' },
+  { label: '公告', value: '2', elTagType: 'success' }
+]
+
+export const NOTICE_STATUS_OPTIONS = [
+  { label: '正常', value: '0', elTagType: 'primary' },
+  { label: '关闭', value: '1', elTagType: 'danger' }
+]
